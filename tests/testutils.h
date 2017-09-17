2691bcfe920543fcfe900478af538623ef55e42b
Bibl_ <GenerallyCool@hotmail.com>
2017-09-18 00:15:00 +0100

    generic-list-feature: added simple test framework
    only contains an assert method for now.

---
+++ tests/testutils.h
@@ -0,0 +1,13 @@
+/*
+ * testutils.h
+ *
+ *  Created on: 17 Sep 2017
+ *      Author: Bibl
+ */
+
+#ifndef TESTS_TESTUTILS_H_
+#define TESTS_TESTUTILS_H_
+
+void assert(int got, int expected, char *msg);
+
+#endif /* TESTS_TESTUTILS_H_ */
